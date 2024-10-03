3561128f8c54838989e3e4960a5110b0c89d3b83
josewilliam-rodriguez <josewrr.1999@gmail.com>
2024-10-03 18:36:44 -0500

    modo pruebas

---
+++ scripts/FormWilliam.js
@@ -0,0 +1,17 @@
+const formulario = document.getElementById('form')
+
+formulario.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    const nombre = document.getElementById('nombre').value
+    const apellidos = document.getElementById('apellido').value
+    const email = document.getElementById('email').value
+    const pasaTiempo = document.getElementById('pasaTiempo').value
+    const horaFecha = document.getElementById('HoraFecha').value
+
+    if (!formulario) {
+            alert('mostrar datos'+ nombre+ apellidos+ email+ pasaTiempo+ horaFecha)
+    } else {
+        alert('ingrese datos')
+    }
+})
