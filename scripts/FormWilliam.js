e4f891dd6857d0fcd30cf48d7fe29662b46aa996
josewilliam-rodriguez <josewrr.1999@gmail.com>
2024-10-03 19:15:35 -0500

    formulario william cambios

--- scripts/FormWilliam.js
+++ scripts/FormWilliam.js
@@ -7,7 +7,15 @@ formulario.addEventListener("submit", (e) => {
     const apellidos = document.getElementById('apellido').value
     const email = document.getElementById('email').value
     const pasaTiempo = document.getElementById('pasaTiempo').value
-    const horaFecha = document.getElementById('HoraFecha').value
+    const horaFecha = document.getElementById('horaFecha').value
 
-    
+
+     const infoParagraph = document.createElement('p');
+
+
+     infoParagraph.textContent = `Nombre: ${nombre}, Apellidos: ${apellidos}, Email: ${email}, Pasa Tiempo: ${pasaTiempo}, Hora y Fecha: ${horaFecha}`;
+
+
+     document.body.appendChild(infoParagraph);
+
 })
